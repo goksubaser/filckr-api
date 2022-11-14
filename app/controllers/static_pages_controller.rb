0b13918a5aa2d26ea468b84c4230c7d5a82651f9
goksubaser <goksubaser@gmail.com>
2022-11-14 18:34:29 +0000

    Api connections are made. It is successfully getin user photo url's

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,30 @@
 class StaticPagesController < ApplicationController
+  def index
+  end
+  def search
+    puts(search_params[:user_id])
+    require 'flickr'
+    flickr = Flickr.new Figaro.env.pusher_key, Figaro.env.pusher_secret
+    list   = flickr.people.getPublicPhotos({api_key: Figaro.env.pusher_key, user_id: search_params[:user_id]})
+
+    @photos = []
+    list.each do |photo|
+      info = flickr.photos.getInfo(:photo_id => photo.id)
+      @photos.push(Flickr.url_o(info))
+      # puts(Flickr.url_o(info))
+    end
+    puts(@photos)
+
+    if !@photos.empty?
+      redirect_to @photos[0], allow_other_host: true
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+  def search_params
+    params.permit(:user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  #
   # Defines the root path route ("/")
-  # root "articles#index"
+  get "/", to: "static_pages#index"
+  post "/", to: "static_pages#search"
+  root "static_pages#index"
 end
